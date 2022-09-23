e41ff64daa0e82682d496e0f9894c89aaaffead0
bicky <bickybong@gmail.com>
2022-09-23 11:56:41 +0800

    keeper part1 done

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>Title of Note</h1>
+      <p>Content of note</p>
+    </div>
+  );
+}
+
+export default Note;
