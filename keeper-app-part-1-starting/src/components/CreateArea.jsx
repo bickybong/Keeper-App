83a1b24425a52e5239e00c9fe9faccec3622b746
bicky <bickybong@gmail.com>
2022-10-14 11:46:30 +0800

    added designs

--- keeper-app-part-1-starting/src/components/CreateArea.jsx
+++ keeper-app-part-1-starting/src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
+  const [clickState, setClickState] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -16,34 +20,44 @@ function CreateArea(props) {
     });
   }
 
+  function changeClickState() {
+    setClickState(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={note.title}
-        />
+        {clickState ? (
+          <input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={note.title}
+          />
+        ) : null}
+
         <textarea
+          onClick={changeClickState}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={clickState ? "3" : "1"}
           value={note.content}
           onChange={handleChange}
         />
-        <button
-          onClick={(e) => {
-            props.addNotes(note);
-            setNote({
-              title: "",
-              content: "",
-            });
-            e.preventDefault();
-          }}
-        >
-          Add
-        </button>
+        <Zoom in={clickState}>
+          <Fab
+            onClick={(e) => {
+              props.addNotes(note);
+              setNote({
+                title: "",
+                content: "",
+              });
+              e.preventDefault();
+            }}
+          >
+            <AddCircleIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
